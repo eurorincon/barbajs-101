c5d7298abae2f4ca6cc5bcaa749546829c1f8087
Euro Rincon <euror@criticalmass.com>
2024-08-22 11:08:08 -0600

    completed my practice

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,37 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
+import { animationEnter, animationLeave } from './animations';
 
+const resetActiveLink = () => gsap.set('a.is-active span', {
+    xPercent: -100,
+    transformOrigin: 'left'
+});
+
+
+
+barba.init({
+    transitions: [
+        {
+            // name: 'home',
+            // to: {
+            //     namespace:['home']
+            // },
+            // sync: true,
+            once({next}) {
+                resetActiveLink();
+                gsap.from('header a', {
+                    duration:0.6,
+                    yPercent: 100,
+                    stagger: 0.2,
+                    ease: 'power1.out'
+                });
+                animationEnter(next.container)
+            },
+            leave: ({current}) => animationLeave(current.container),
+            enter({next}) {
+                animationEnter(next.container)
+            },
+
+        }
+    ]
+})
